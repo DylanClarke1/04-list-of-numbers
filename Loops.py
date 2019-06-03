3a3164160b2ecd96d69a626382ce2fcee7d940ea
DylanClarke1 <49927178+DylanClarke1@users.noreply.github.com>
2019-06-02 21:12:13 -0600

    Update Loops.py

--- Loops.py
+++ Loops.py
@@ -1,9 +1,13 @@
 nums = [12, 10, 32, 3, 66, 17, 42, 99, 20]
 
 # Write a loop that prints each of the numbers on a new line.
+line1 = ""
 for x in nums:
-  print(x)
+    line1 += str(x) + " "
+print(line1)
 
 # Write a second loop that prints each number and its square on a new line.
+line2 = ""
 for x in nums:
-  print("The square of " + str(x) + " is " + str(x*x)) 
+    line2 += "The square of " + str(x) + " is " + str(x*x) + " "
+print(line2)
