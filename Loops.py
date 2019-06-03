108b74394fd1e693e0ebadc924bcdaab540e15b1
DylanClarke1 <49927178+DylanClarke1@users.noreply.github.com>
2019-06-02 20:54:00 -0600

    Create Loops.py

---
+++ Loops.py
@@ -0,0 +1,9 @@
+nums = [12, 10, 32, 3, 66, 17, 42, 99, 20]
+
+# Write a loop that prints each of the numbers on a new line.
+for x in nums:
+  print(x)
+
+# Write a second loop that prints each number and its square on a new line.
+for x in nums:
+  print("The square of " + str(x) + " is " + str(x*x)) 
